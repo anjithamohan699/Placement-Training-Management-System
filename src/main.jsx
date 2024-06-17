8cde85abe8b421f0ba9ca2991c8f2965717d92e2
Anjitha Mohan <anjithaaisat@gmail.com>
2024-06-17 15:35:15 +0530

    replace BrowserRouter with HashRouter to avoid 404 error

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { CookiesProvider } from 'react-cookie'
 
 const root = ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CookiesProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
     </CookiesProvider>
   </React.StrictMode>,
 )
